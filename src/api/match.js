baedc9f0850444801f7a3277932ac823c25fa91d
Tintin Falk <tintinfalk01@gmail.com>
2024-12-06 11:34:26 +0100

    player can only draw card in start of the turn

--- src/api/match.js
+++ src/api/match.js
@@ -23,6 +23,21 @@ export async function fetchMatchData() {
     }
 }
 
+export async function fetchStartTurn(playerId)
+{
+    try {
+        const response = await fetch(`https://localhost:44393/startTurn?PlayerId=${playerId}`, {
+            method: 'GET'
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch start turn, wrong player!');
+        }
+        return await response.text();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 export async function fetchEndTurn(playerId) {
     try {
         const response = await fetch(`https://localhost:44393/endTurn?playerId=${playerId}`, {
